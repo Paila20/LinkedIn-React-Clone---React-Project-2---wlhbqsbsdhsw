9b1b677354d890a85d04a106938e338960ba3261
paila20 <rajarajeswar169@gmail.com>
2024-03-28 18:20:29 +0530

    changes done

--- src/components/common/ProfileCard/index.jsx
+++ src/components/common/ProfileCard/index.jsx
@@ -1,76 +1,133 @@
-import React, { useState, useMemo } from "react";
+// import React, { useState, useMemo } from "react";
 
+// import PostsCard from "../PostsCard";
+// import { HiOutlinePencil } from "react-icons/hi";
+
+// import "./index.scss";
+// import PostStatus from "../PostUpdate";
+
+// export default function ProfileCard({
+//   // onEdit,
+//   currentUser,
+//   posts
+// }) {
+//   return (
+//     <>
+//       <div className="profile-card">
+//         <div className="profile-info">
+//           <div>
+//           {currentUser?.data?._id === posts?.author._id?(
+//             <>
+//             {currentUser?.data?.profileImage ? (
+//               <img
+//                 className="profile-image"
+//                 src={currentUser.data.profileImage}
+//                 alt="profile-image"
+//               />
+//             ) : (
+//               <img
+//                 className="profile-image"
+//                 src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+//                 alt="profile-image"
+//               />
+//             )}
+
+//             <h3 className="userName">{currentUser?.data?.name}</h3>
+//             <p className="heading">{currentUser?.data?.email}</p>
+//             </>
+//           ):(
+//             <>
+//             {posts?.author?.profileImage ? (
+//               <img
+//                 className="profile-image"
+//                 src={posts?.author?.profileImage}
+//                 alt="profile-image"
+//               />
+//             ) : (
+//               <img
+//                 className="profile-image"
+//                 src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+//                 alt="profile-image"
+//               />
+//             )}
+
+//             <h3 className="userName">{posts?.author?.name}</h3>
+//             <p className="heading">{posts?.author?.email}</p>
+//           )
+//             }
+//             </>
+//           </div>
+//         </div>
+//       </div>
+
+     
+//       <div className="post-status-main">
+
+//         <PostStatus currentUser={currentUser} postingCheck={false} />
+//         {/* {allPosts?.map((posts) => {
+//           return (
+//             <div key={posts._id}>
+//               <PostsCard posts={posts} />
+//             </div>
+//           );
+//         })} */}
+//       </div>
+//     </>
+//   );
+// }
+import React from "react";
 import PostsCard from "../PostsCard";
 import { HiOutlinePencil } from "react-icons/hi";
-
 import "./index.scss";
 import PostStatus from "../PostUpdate";
 
-export default function ProfileCard({
-  // onEdit,
-  currentUser,
-  // posts
-}) {
+export default function ProfileCard({ currentUser, posts }) {
   return (
     <>
       <div className="profile-card">
         <div className="profile-info">
           <div>
-            {currentUser?.data?.profileImage ? (
-              <img
-                className="profile-image"
-                src={currentUser.data.profileImage}
-                alt="profile-image"
-              />
+            {currentUser?.data?._id === posts?.author._id ? (
+              <>
+                {currentUser?.data?.profileImage ? (
+                  <img
+                    className="profile-image"
+                    src={currentUser.data.profileImage}
+                    alt="profile-image"
+                  />
+                ) : (
+                  <img
+                    className="profile-image"
+                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                    alt="profile-image"
+                  />
+                )}
+                <h3 className="userName">{currentUser?.data?.name}</h3>
+                <p className="heading">{currentUser?.data?.email}</p>
+              </>
             ) : (
-              <img
-                className="profile-image"
-                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                alt="profile-image"
-              />
+              <>
+                {posts?.author?.profileImage ? (
+                  <img
+                    className="profile-image"
+                    src={posts?.author?.profileImage}
+                    alt="profile-image"
+                  />
+                ) : (
+                  <img
+                    className="profile-image"
+                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                    alt="profile-image"
+                  />
+                )}
+                <h3 className="userName">{posts?.author?.name}</h3>
+                <p className="heading">{posts?.author?.email}</p>
+              </>
             )}
-
-            <h3 className="userName">{currentUser?.data?.name}</h3>
-            <p className="heading">{currentUser?.data?.email}</p>
           </div>
         </div>
       </div>
-
-      {/* <div className="profile-card">
-        
-        <div className="profile-info">
-          <div>
-            <img
-              className="profile-image"
-          
-              src={
-                Object.values(currentProfile).length === 0
-                  ? currentUser.imageLink
-                  : currentProfile?.imageLink
-              }
-              alt="profile-image"
-            />
-            <h3 className="userName">
-              {Object.values(currentProfile).length === 0
-                ? currentUser.name
-                : currentProfile?.name}
-            </h3>
-            <p className="heading">
-              {Object.values(currentProfile).length === 0
-                ? currentUser.headline
-                : currentProfile?.headline}
-            </p>
-           
-           
-          </div>
-
-         
-        </div>
-       
-
-       
-      </div> */}
-
+     
       <div className="post-status-main">
         <PostStatus currentUser={currentUser} postingCheck={false} />
         {/* {allPosts?.map((posts) => {
