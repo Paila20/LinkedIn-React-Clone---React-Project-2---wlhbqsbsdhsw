53467bc14e1a647836545c0bb80789f6d15cd51e
paila20 <rajarajeswar169@gmail.com>
2024-04-29 14:28:56 +0530

    updated the searchcard

--- src/helpers/timeStampConversion.jsx
+++ src/helpers/timeStampConversion.jsx
@@ -5,10 +5,7 @@ export const timeStampConversionToDateAndTime = (timestamp) => {
       year: "numeric",
       month: "long",
       day: "numeric",
-      // hour: "2-digit",
-      // minute: "2-digit",
-      // second: "2-digit",
-      // hour12: true,
+     
     };
     return new Intl.DateTimeFormat("en-US", options).format(date);
   } else {
